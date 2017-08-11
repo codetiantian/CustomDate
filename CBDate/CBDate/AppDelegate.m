e65482cdc19118d991c9911ede3c2a8495714e24
ncimall <1655902151@qq.com>
2017-08-11 16:42:08 +0800

    Revert "修改代码"
    This reverts commit f69c5772397e4ed1698034b89d5e1b2eacda5b7d.

--- CBDate/CBDate/AppDelegate.m
+++ CBDate/CBDate/AppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"测试一下");
     return YES;
 }
 
