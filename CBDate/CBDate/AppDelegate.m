ac65d738dd53e328870817043e2d8a7cd2c8a9e2
ncimall <1655902151@qq.com>
2017-08-11 17:04:33 +0800

    Revert "提交"
    This reverts commit c0836f1fc5cc0bd4c62f90a3039b7f9aef070e0b.

--- CBDate/CBDate/AppDelegate.m
+++ CBDate/CBDate/AppDelegate.m
@@ -17,9 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    
-    NSLog(@"hello 你好");
-    
     return YES;
 }
 
--- CBDate/CBDate/ViewController.m
+++ CBDate/CBDate/ViewController.m
@@ -71,8 +71,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.view.backgroundColor = [UIColor greenColor];
-    
     self.dateFormatter = [[NSDateFormatter alloc] init];
     self.dateFormatter.dateFormat = @"yyyyMM";
     
